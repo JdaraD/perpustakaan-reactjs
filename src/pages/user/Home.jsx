bd031354871ebe3f07f0d31f09d5b0fea16af5f0
Julian Diwandara <juliandara17@gmail.com>
2026-06-05 18:50:07 +0700

    update fix template home page done

--- src/pages/user/Home.jsx
+++ src/pages/user/Home.jsx
@@ -4,11 +4,11 @@ import image from "../../../public/img/background.jpg";
 
 function kotakPerulangan() {
   const kotak = [];
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 10; i++) {
     kotak.push(
       <div
         key={i}
-        className="flex w-68 h-24 bg-green-500 gap-2 shadow-lg rounded-md p-2"
+        className="shrink-0 flex w-68 h-24 bg-green-500 gap-2 rounded-md p-2"
       >
         <div className="relative flex justify-center items-center w-1/2 rounded-md bg-amber-400">
           <div className="absolute w-18 h-24 bottom-2 rounded-md shadow-xl bg-red-400"></div>
@@ -29,7 +29,7 @@ function kotakPerulangan() {
 
 function NewRelease() {
   const kotak = [];
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 20; i++) {
     kotak.push(
       <div
         key={i}
@@ -40,13 +40,13 @@ function NewRelease() {
         </div>
 
         <div className="flex flex-col items-center gap-2 p-4">
-          <div className="text-center text-gray-800">
-            <p>Harry Potter</p>
-            <p>2019</p>
-            <p>Charles</p>
+          <div className="text-center text-sm text-gray-800">
+            <p className="text-base font-bold">Harry Potter</p>
+            <p className="text-sm font-semibold">2019</p>
+            <p className="text-xs font-normal">Charles</p>
           </div>
 
-          <button className="bg-amber-500 text-xs font-bold text-white py-1 px-4 rounded-md hover:bg-amber-600">
+          <button className="bg-amber-500 text-xs font-bold text-white py-1 px-4 rounded-md hover:bg-amber-600 cursor-pointer">
             View Details
           </button>
         </div>
@@ -58,94 +58,64 @@ function NewRelease() {
 
 const Home = () => {
   return (
-    <div className="flex flex-col h-auto py-4 w-full items-center gap-4">
-      <div className="flex w-[90%]">
-        <div className="flex w-full h-full shadow-lg overflow-hidden">
-          <div className="w-[44%] h-86 bg-amber-600 rounded-l-md shadow-lg p-6 flex flex-col gap-4 justify-center">
-            <h1 className="text-3xl font-bold text-gray-800">
+    <div className="container mx-auto px-4 py-6 flex flex-col gap-8">
+      {/* HERO SECTION */}
+      <section className="w-full">
+        <div className="flex flex-col lg:flex-row overflow-hidden rounded-xl shadow-lg">
+          {/* Content */}
+          <div className=" w-full lg:w-[45%] bg-amber-600 p-6 lg:p-8 flex flex-col justify-center gap-4">
+            <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold text-gray-800">
               Welcome to BookVerse, your ultimate destination for all things
               books!
             </h1>
-            <p className="capitalize text-justify text-white">
+
+            <p className="text-sm md:text-base text-white text-justify">
               Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since 1966, when designers at Letraset and James Mosley, the
               librarian at St Bride Printing Library, took a 1914 Cicero
               translation and scrambled it to make dummy text for Letraset's
-              Body Type sheets. It has survived not only many decades, but also
-              the leap into electronic typesetting, remaining essentially
-              unchanged. It was popularised thanks to these sheets and more
-              recently with desktop publishing software including versions of
-              Lorem Ipsum.
+              Body Type sheets.
             </p>
           </div>
+
+          {/* Image */}
           <img
             src={image}
             alt="Background"
-            className="w-[56%] h-86 object-cover rounded-r-md shadow-lg"
+            className=" w-full lg:w-[55%]h-62.5 md:h-87.5 lg:h-auto object-cover"
           />
         </div>
-      </div>
+      </section>
 
-      <div className="flex flex-col w-full items-center gap-6">
-        <div className="flex w-[90%] h-auto items-center justify-start">
-          <h2 className="text-xl font-bold">Most viewed</h2>
-        </div>
-        <div className="flex w-[90%] h-auto gap-4 bg-gray-200 rounded-md shadow-lg">
-          {kotakPerulangan()}
-          {/* <div className="flex w-68 h-24 bg-green-500 gap-2 shadow-lg rounded-md p-2">
-              <div className="relative flex justify-center items-center w-1/2 rounded-md bg-amber-400">
-                <div className="absolute w-18 h-24 bottom-2 rounded-md shadow-xl bg-red-400"></div>
-              </div>
+      <section className="w-full flex flex-col gap-4">
+        <h2 className="text-xl md:text-2xl font-bold">Most Viewed</h2>
 
-              <div className="flex w-1/2 rounded-md bg-blue-400">
-                <ul className="list-disc flex flex-col justify-center pl-5 text-white text-sm">
-                  <li>Harry Potter</li>
-                  <li>2019</li>
-                  <li>Charles</li>
-                </ul>
-              </div>
-            </div> */}
+        <div className="bg-gray-200 rounded-xl shadow-lg p-4 overflow-x-auto custom-scrollbar ">
+          <div className="flex gap-4 min-w-max">{kotakPerulangan()}</div>
         </div>
-      </div>
+      </section>
 
-      <div className="flex flex-col w-full items-center gap-6">
-        <div className="flex w-[90%] h-auto items-center justify-start">
-          <h2 className="text-xl font-bold">New Release</h2>
-        </div>
-        <div className="flex flex-col w-[90%] h-300 p-4 gap-4 bg-gray-200 rounded-md shadow-lg">
-          <div className="flex w-full h-20 bg-amber-600 rounded-md"></div>
-          <div className="flex w-full h-full gap-4">
-            <div className="flex-1 bg-blue-300 rounded-md p-4 overflow-auto">
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-5 3xl:grid-cols-6 gap-4">
+      <section className="w-full flex flex-col gap-4">
+        <h2 className="text-xl md:text-2xl font-bold">New Release</h2>
+
+        <div className="bg-gray-200 rounded-xl shadow-lg p-4">
+          <div className="h-20 bg-amber-600 rounded-lg mb-4"></div>
+
+          <div className="flex flex-col xl:flex-row gap-4">
+            <div className="flex-1 bg-blue-300 rounded-lg p-4">
+              <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(180px,1fr))]">
                 {NewRelease()}
-                {/* <div className="flex flex-col bg-white rounded-md shadow-lg overflow-hidden">
-                  <div className="flex justify-center items-center bg-red-400">
-                    <div className="w-36 h-40 bg-amber-300"></div>
-                  </div>
-
-                  <div className="flex flex-col items-center gap-2 p-4">
-                    <div className="text-center text-gray-800">
-                      <p>Harry Potter</p>
-                      <p>2019</p>
-                      <p>Charles</p>
-                    </div>
-
-                    <button className="bg-amber-500 text-xs font-bold text-white py-1 px-4 rounded-md hover:bg-amber-600">
-                      View Details
-                    </button>
-                  </div>
-                </div> */}
               </div>
             </div>
 
             {/* Sidebar */}
-            <aside className="hidden lg:block w-72 shrink-0">
-              <div className="sticky top-20 h-100 bg-purple-300 rounded-md"></div>
+            <aside className="w-full xl:w-72 shrink-0">
+              <div className="xl:sticky xl:top-20 h-75 xl:h-100 bg-purple-300 rounded-lg" />
             </aside>
           </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 };
